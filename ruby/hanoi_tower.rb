8c31a509c04d0dd6a41bb086089c98e92fd6abfe
Fabio 'c0m3tx' Vitale <cometspk@gmail.com>
2021-04-29 14:53:33 +0200

    Simpler ruby version

--- ruby/hanoi_tower.rb
+++ ruby/hanoi_tower.rb
@@ -1,12 +1,8 @@
-a = Object.new.freeze
-b = Object.new.freeze
-c = Object.new.freeze
-
 def hanoi(n, from, mid, to)
   return 1 if n == 1
 
   hanoi(n - 1, from, to, mid) + 1 + hanoi(n - 1, mid, from, to)
 end
 
-result = hanoi(32, a, b, c)
+result = hanoi(32, :a, :b, :c)
 puts "#{result} moves"
