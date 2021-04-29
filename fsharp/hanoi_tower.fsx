fca543807e8f7128a058f4129d34bf1f18c6c52b
Fabio 'c0m3tx' Vitale <cometspk@gmail.com>
2021-04-29 15:42:54 +0200

    Fix F# type

--- fsharp/hanoi_tower.fsx
+++ fsharp/hanoi_tower.fsx
@@ -1,7 +1,7 @@
-let rec Hanoi n from mid dest = 
+let rec Hanoi n from mid dest : uint64 = 
    match n with
-   | 1 -> 1
-   | n -> (Hanoi (n - 1) from dest mid) + 1 + (Hanoi (n - 1) mid from dest) 
+   | 1 -> 1UL
+   | n -> (Hanoi (n - 1) from dest mid) + 1UL + (Hanoi (n - 1) mid from dest) 
 
 let value = (Hanoi 32 'a' 'b' 'c')
 printfn "Moves: %d" value
