4f278fd8d462ff6a4a77aa8d246229247d3f40dc
Fabio 'c0m3tx' Vitale <cometspk@gmail.com>
2025-11-11 20:11:46 +0100

    haskell: add typing for towers

--- haskell/hanoi_tower.hs
+++ haskell/hanoi_tower.hs
@@ -1,9 +1,9 @@
-hanoi :: Int -> Int -> Int -> Int -> Int
+data Tower = A | B | C deriving Show
+
+hanoi :: Int -> Tower -> Tower -> Tower -> Int
 hanoi 1 _ _ _ = 1
-hanoi n x y z = start_to_pass + 1 + pass_to_destination
-  where
-    start_to_pass = (hanoi (n-1) x z y)
-    pass_to_destination = (hanoi (n-1) y x z)
+hanoi n x y z = 
+  (hanoi (n - 1) x z y) + 1 + (hanoi (n - 1) y x z)
 
 main = do
-  putStrLn $ "Moves: " ++ show (hanoi 32 1 2 3)
+  putStrLn $ "Moves: " ++ show (hanoi 32 A B C)
